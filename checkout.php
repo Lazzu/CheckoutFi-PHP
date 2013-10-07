f66b550c7e4f0754e954f050a454e6a6aba1b424
Lasse Numminen <lasse.numminen@lasoft.fi>
2013-10-07 23:26:35 +0300

    Changed the licence in the checkout.php file to correct one (MIT)

--- checkout.php
+++ checkout.php
@@ -10,33 +10,8 @@
  * Created by Lasse Numminen
  * http://lasse.pw/
  *
- * Gnu GPL V3
+ * MIT Licence
  *
-
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-	Roadmap:
-
-	* 1.0.0 - Support for payment polls, support for old deprecated method
-
-	Changelog:
-
-	* 2013-10-7 - 0.1.0 - Initial release. Support for basic checkouts.
-
-	Interface API may change in between major versions.
-
  **/
 
 namespace CheckoutFi;
